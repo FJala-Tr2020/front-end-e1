83540ac5e8fe5d88721441db98e5a68c2bc7c39c
douglasalizares <dery.alizares@gmail.com>
2021-07-09 01:41:03 +0000

    The routers for navigation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as ko from 'knockout';
+import * as Sammy from 'sammy';
 require('knockout.validation');
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,13 +7,40 @@ import LeadTable from './pages/leadTable';
 
 ko.components.register('lead-table', LeadTable);
 class AppViewModel {
-    lead: {url:string}
-    constructor(){
-        this.lead = {
-            url: 'leadTable.html'
-        }
-    }
+  lead: { url: string };
+  constructor() {
+    this.lead = {
+      url: 'leadTable.html'
+    };
+  }
 }
-// console.log('object')
 
+class AppMainViewModel {
+  tableLeadsVisible: ko.Observable<boolean>;
+
+  constructor() {
+    // These values are hard-coded but could come from a server API request with JSON response
+    this.tableLeadsVisible = ko.observable(false);
+  }
+  showVisibleTableLEads(): void {
+    console.log('Esta visible!!!');
+    this.tableLeadsVisible(true);
+  }
+  hiddenTableLeads(): void {
+    console.log('Esta invisible!!!');
+    this.tableLeadsVisible(false);
+  }
+}
+const appPrincipal = new AppMainViewModel();
+
+const app: Sammy.Application = Sammy();
+app.get('#/foobar', () => {
+  appPrincipal.showVisibleTableLEads();
+});
+app.get('#/goToHome', () => {
+  appPrincipal.hiddenTableLeads();
+});
+app.run();
+
+ko.applyBindings(appPrincipal, document.getElementById('mainView'));
 ko.applyBindings(new AppViewModel(), document.getElementById('newPage'));
