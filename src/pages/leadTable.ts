9ad1dd4864ca453cd40883b0f1e80f0dc7d3e35c
An70 <antonionavapaz@outlook.com>
2021-07-09 13:01:51 -0400

    Fixing observations about comments, Spanish funtion names

--- src/pages/leadTable.ts
+++ src/pages/leadTable.ts
@@ -4,26 +4,19 @@ import { Grid } from 'gridjs';
 class LeadTable {
   response = {};
   constructor() {
-
     this.response = new Grid({
       columns: ['Creation Date', 'Title', 'Name', 'Email', 'Phone', 'City', 'Country', 'Language'],
       server: {
-        //url: 'https://api.scryfall.com/cards/search?q=Inspiring',
         url: 'https://api.scryfall.com/cards/search?q=Inspiring',
         data: (opts) => {
           return new Promise((resolve, reject) => {
-            // let's implement our own HTTP client
             const xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
               if (this.readyState === 4) {
                 if (this.status === 200) {
                   const resp = JSON.parse(this.response);
-
-                  // make sure the output conforms to StorageResponse format: 
-                  // https://github.com/grid-js/gridjs/blob/master/src/storage/storage.ts#L21-L24
                   resolve({
-                    data: resp.data.map(lead => [lead.released_at, lead.rarity, lead.name, lead.artist + '@mail.com', lead.collector_number+'-333', 'La Paz', lead.artist, lead.lang]),
-                    //data: resp.map(card => [lead.released_at, lead.rarity, lead.name, lead.artist + '@mail.com', lead.collector_number+'-333', 'La Paz', lead.artist, lead.lang]),
+                    data: resp.data.map(lead => [lead.released_at, lead.rarity, lead.name, lead.artist + '@mail.com', lead.collector_number + '-333', 'La Paz', lead.artist, lead.lang]),
                     total: resp.total_cards,
                   });
                 } else {
