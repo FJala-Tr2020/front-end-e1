efd1b33dd833322d26ea8f18651e24c662896b69
An70 <antonionavapaz@outlook.com>
2021-07-20 01:41:08 -0400

    change of rest client to axios due to at first was using another rest client

--- src/pages/leadTable.ts
+++ src/pages/leadTable.ts
@@ -1,39 +1,28 @@
 import template from '../pages/leadTable.html';
 import { Grid } from 'gridjs';
+import axios from 'axios';
+interface ILead {
+  released_at:string
+  name:string
+  artist:string
 
+}
 class LeadTable {
   response = {};
   constructor() {
-
     this.response = new Grid({
       columns: ['Creation Date', 'Title', 'Name', 'Email', 'Phone', 'City', 'Country', 'Language'],
       server: {
-        //url: 'https://api.scryfall.com/cards/search?q=Inspiring',
-        url: 'http://localhost:3000/leads',
-        data: (opts) => {
-          return new Promise((resolve, reject) => {
-            // let's implement our own HTTP client
-            const xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-              if (this.readyState === 4) {
-                if (this.status === 200) {
-                  const resp = JSON.parse(this.response);
-                  console.log('---------------------->');
-                  console.log(resp);
-                  // make sure the output conforms to StorageResponse format:
-                  // https://github.com/grid-js/gridjs/blob/master/src/storage/storage.ts#L21-L24
-                  resolve({
-                    data: resp.leads.map(lead => [lead.createDate, lead.title, lead.contactName, lead.email, lead.phone, lead.city, lead.country, lead.language]),
-                    //data: resp.map(card => [lead.released_at, lead.rarity, lead.name, lead.artist + '@mail.com', lead.collector_number+'-333', 'La Paz', lead.artist, lead.lang]),
-                    total: 2
-                  });
-                } else {
-                  reject();
-                }
-              }
-            };
-            xhttp.open("GET", opts.url, true);
-            xhttp.send();
+        data: () => {
+          return new Promise((resolve) => {
+            //'http://localhost:3000/leads',
+            axios.get('https://api.scryfall.com/cards/search?q=Inspiring').then(({ data }) => {
+              resolve({
+                // data: data.data.map(lead => [lead.createDate, lead.title, lead.contactName, lead.email, lead.phone, lead.city, lead.country, lead.language]),
+                data: data.data.map((lead:ILead) => [lead.released_at, lead.name, lead.artist, lead.name+'@mail.com',11111, '2',3,"ESP"]),
+                total: 2
+              });
+            });
           });
         }
       }
