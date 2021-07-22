32482adaca446511ae9e6a78252687427d5922f6
An70 <antonionavapaz@outlook.com>
2021-07-21 20:00:38 -0400

    hiding the table element using visible binding * needs to add the leads table

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
 import * as ko from 'knockout';
 require('knockout.validation');
-import 'bootstrap/dist/css/bootstrap.min.css';
-import LeadTable from './pages/leadTable';
 
-ko.components.register('lead-table', LeadTable);
-class AppViewModel {
-  lead: { url: string }
-  constructor() {
-    this.lead = {
-      url: 'leadTable.html'
-    }
+interface Iregistro {
+  id:number
+  nombre:string
+  skill:string
+}
+class ViewModel {
+  registros = [
+    { id: 1, nombre: "Name1", skill: "skill 1" },
+    { id: 2, nombre: "Name2", skill: "Skill 2" },
+    { id: 3, nombre: "Name3", skill: "Skill 3" },
+  ];
+  nombre:ko.Observable<string>;
+  edad:ko.Observable<number>;
+  gusto:ko.Observable<string>;
+  isDisplayed:ko.Observable<boolean>;
+  constructor(nombre:string, edad:number) {
+    this.nombre = ko.observable(nombre);
+    this.edad = ko.observable(edad);
+    this.gusto = ko.observable(`<h1>Element</h1>`);
+    this.isDisplayed = ko.observable(false);
+  }
+  show() {
+    this.isDisplayed(true);
+  }
+  hide() {
+    this.isDisplayed(false);
   }
 }
-ko.applyBindings(new AppViewModel(), document.getElementById('newPage'));
+ko.applyBindings(new ViewModel("MiNombre", 131));
